eb31a44c04b5b2af40d3ecede49d9dffa5695170
jejecrunch <gkswlgp456@gmail.com>
2018-09-03 19:46:28 +0900

    plan 02. 수영장 회원 관리 시스템

---
+++ src/Plan02.java
@@ -0,0 +1,26 @@
+
+public class Plan02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("plan02 : 201632035 한지혜");
+		System.out.println("주제 :: 수영장 회원 관리 프로그램\n");
+		System.out.println("****************************************\n");
+		System.out.println("1. 초기화면 : 로그인 화면");
+		System.out.println("1-1. 관리자 로그인");
+		System.out.println("1-2. 일반회원 로그인\n");
+		System.out.println("2. 회원가입 화면");
+		System.out.println("3. 로그인 후 화면");
+		System.out.println("3-1. 관리자 : 회원리스트, 위에 버튼으로 반별로 나뉜 회원 보기 가능, 결제 취소 버튼");
+		System.out.println("3-2. 일반회원 : 회원번호+회원이름 뜨면서 환영합니다 뜨고 이후에 남은 수강일수, 마지막으로 결제한 날 뜸\n");
+		System.out.println("4. 회원 정보 관리");
+		System.out.println("4-1. 관리자 : 관리자 본인의 정보는 수정 불가능, 결제 메뉴 누르면 결제 취소할 수 있는 버튼, 회원 탈퇴 버튼");
+		System.out.println("4-2. 일반회원 : 회원 수정에 들어가면 정보 수정 가능, 결제 메뉴 누르면 결제 여부 뜨고 시스템 내에서 결제 가능, 구글 캘린더와 연동 여부, 회원 탈퇴 버튼\n");
+		System.out.println("****************************************\n");
+		System.out.println("필요한 기능 :: 로그인(일반회원, 관리자), 회원가입, 로그인 후에 화면");
+		System.out.println("필요한 정보(회원) :: 회원 이름, 회원 번호, 회원 아이디, 회원 비밀번호, 수강일수, 결제여부, 구글캘린더와 연동해서 자동 알람 등록, 회원 탈퇴");
+		System.out.println("필요한 정보(관리자) :: 관리자 아이디, 관리자 비밀번호, 회원 리스트 (처음엔 최신순, 그러나 반별로 카테고리 나눠서), 결제 취소\n");
+		System.out.println("****************************************\n");
+	}
+
+}
